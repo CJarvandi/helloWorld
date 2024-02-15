069943d00e68190f3b7c2083f80096aac5a292c7
Cjninja <cameronjarvandi@gmail.com>
2024-02-14 20:15:11 -0500

    Making an About Me page.

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World! Cameron Jarvandi I am adding my first code change'
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == '__main__':
     app.run()
