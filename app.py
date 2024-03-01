53888f5e4cb23528f6dc62fcc5f2206f2a546d0d
Cjninja <cameronjarvandi@gmail.com>
2024-02-29 20:12:06 -0500

    Created some CSS rules and modified my HTML

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World! Cameron Jarvandi I am adding my first code change'
 
-@app.route('/about')
+@app.route('/about-css')
 def about():
-    return render_template('about.html')
+    return render_template('about-css.html')
 if __name__ == '__main__':
     app.run()
