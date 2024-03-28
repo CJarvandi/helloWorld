9bd524f35911cd12964021f969c5aa168bcfa593
Cjninja <cameronjarvandi@gmail.com>
2024-03-28 17:39:02 -0400

    fixed code so url provides information on page for favorite-course. Got contact page to work also

--- app.py
+++ app.py
@@ -10,18 +10,23 @@ def hello_world():  # put application's code here
 @app.route('/about-css')
 def about():
     return render_template('about-css.html')
-if __name__ == '__main__':
-    app.run()
 
 @app.route('/favorite-course')
 def fCourse():
     print('Subject: ' + request.args.get('Subject'))
-    print('Course_number: ' + request.args.get('Course Number'))
+    print('Course_number: ' + request.args.get('Course_Number'))
     return render_template('favorite-course.html')
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     if request.method == 'POST':
+        print('First name entered:' + request.form.get('first_name'))
+        print('Last name entered:' + request.form.get('last_name'))
+        print('Email entered:' + request.form.get('email'))
+    if request.method == '':
         return render_template('contact.html', form_submitted=True)
     else:
         return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run()
